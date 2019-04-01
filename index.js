a61e33aaa0533b5da3a0dbbd7a31d1f3077bcabf
Alex Acker <alex.acker@gonitro.com>
2019-04-02 09:57:45 +1100

    A bit of file structure rearrangement

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const fs = require('fs');
 const csv = require('csvtojson');
 const formatXml = require('xml-formatter');
-const csvFile = './sitemap-csv/full-site.csv';
+const csvFile = './csv/full-site.csv';
 const langs = ['en','de','es','fr','nl','it'];
-const formatXmlOptions = {indentation:'  ',collapseContent:false};
+const formatXmlOptions = { indentation:'  ', collapseContent:false };
 
 function handleErr(err){
     if(err)console.log(err);
     else console.log('File done');
 }
+
 function buildXml(obj){
     langs.forEach(lang => {
         const xmlns = 'http://www.sitemaps.org/schemas/sitemap/0.9';
@@ -20,8 +21,7 @@ function buildXml(obj){
             <url><loc>https://www.gonitro.com${lang === 'en' ? '' : `/${lang}`}${page.url}</loc><lastmod>${page.lastmod}</lastmod><changefreq>${page.changefreq}</changefreq><priority>${page.priority}</priority></url>`
         });
         body += `</urlset>`;
-        console.log(body);
-        fs.writeFileSync(`./sitemap-csv/sitemap-${lang}.xml`,formatXml(body.trim(),formatXmlOptions),handleErr);
+        fs.writeFileSync(`./sitemaps/sitemap-${lang}.xml`,formatXml(body.trim(),formatXmlOptions),handleErr);
     });
 }
 
